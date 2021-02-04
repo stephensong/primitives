71d199d19a1d63aa51e68821a5e44be683b404ca
Benoît Grélard <benoit.grelard@gmail.com>
2021-02-04 18:10:02 +0000

    [Separator] Add chromatic story

--- packages/react/separator/src/Separator.stories.tsx
+++ packages/react/separator/src/Separator.stories.tsx
@@ -1,6 +1,6 @@
 import * as React from 'react';
 import { Separator } from './Separator';
-import { styled } from '../../../../stitches.config';
+import { css } from '../../../../stitches.config';
 
 export default { title: 'Components/Separator' };
 
@@ -8,27 +8,55 @@ export const Styled = () => (
   <>
     <h1>Horizontal</h1>
     <p>The following separator is horizontal and has semantic meaning.</p>
-    <Separator as={StyledRoot} orientation="horizontal" />
+    <Separator className={rootClass} orientation="horizontal" />
     <p>
       The following separator is horizontal and is purely decorative. Assistive technology will
       ignore this element.
     </p>
-    <Separator as={StyledRoot} orientation="horizontal" decorative />
+    <Separator className={rootClass} orientation="horizontal" decorative />
 
     <h1>Vertical</h1>
     <div style={{ display: 'flex', alignItems: 'center' }}>
       <p>The following separator is vertical and has semantic meaning.</p>
-      <Separator as={StyledRoot} orientation="vertical" />
+      <Separator className={rootClass} orientation="vertical" />
       <p>
         The following separator is vertical and is purely decorative. Assistive technology will
         ignore this element.
       </p>
-      <Separator as={StyledRoot} orientation="vertical" decorative />
+      <Separator className={rootClass} orientation="vertical" decorative />
     </div>
   </>
 );
 
-const StyledRoot = styled('div', {
+export const Chromatic = () => (
+  <>
+    <h1>Horizontal</h1>
+    <p>The following separator is horizontal and has semantic meaning.</p>
+    <Separator className={rootClass} orientation="horizontal" />
+    <p>
+      The following separator is horizontal and is purely decorative. Assistive technology will
+      ignore this element.
+    </p>
+    <Separator className={rootClass} orientation="horizontal" decorative />
+
+    <h1>Vertical</h1>
+    <div style={{ display: 'flex', alignItems: 'center' }}>
+      <p>The following separator is vertical and has semantic meaning.</p>
+      <Separator className={rootClass} orientation="vertical" />
+      <p>
+        The following separator is vertical and is purely decorative. Assistive technology will
+        ignore this element.
+      </p>
+      <Separator className={rootClass} orientation="vertical" decorative />
+    </div>
+
+    <h1>Data attribute selectors</h1>
+    <Separator className={rootAttrClass} />
+  </>
+);
+Chromatic.parameters = { chromatic: { disable: false } };
+
+const rootClass = css({
   border: 'none',
   backgroundColor: '$red',
 
@@ -44,3 +72,10 @@ const StyledRoot = styled('div', {
     margin: '0 20px',
   },
 });
+
+const styles = {
+  backgroundColor: 'rgba(0, 0, 255, 0.3)',
+  border: '2px solid blue',
+  padding: 10,
+};
+const rootAttrClass = css({ '&[data-radix-separator]': styles });
